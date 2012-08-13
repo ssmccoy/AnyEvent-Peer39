46fb0858a7fc7c0c6cd1ddaf630d2ec79691aa29
Scott S. McCoy <smccoy@saymedia.com>
2012-08-13 11:24:29 -0700

    Added Test::More to build requirements

--- lib/AnyEvent/Peer39.pm
+++ lib/AnyEvent/Peer39.pm
@@ -7,7 +7,7 @@ use AnyEvent::HTTP;
 use Data::Validator;
 use Mouse::Util::TypeConstraints;
 
-our $VERSION = "0.31";
+our $VERSION = "0.32";
 
 has base_url     => ( is => 'ro', isa => 'Str', required => 1,);
 has api_key      => ( is => 'ro', isa => 'Str', required => 1,);
